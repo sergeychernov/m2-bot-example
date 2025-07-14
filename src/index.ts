f2bf83b7e89231e4d98ebab304b534e59313bcb4
sergeevaam <sergeevaam@sergeevaam-mbpm3pro.local>
2025-07-14 15:53:52 +0300

    chore: add media messages handling

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ import { Api, Bot, Context, RawApi } from 'grammy';
 import { initializeActivateCommand } from './activate-command';
 import { Message } from 'grammy/types';
 import { getUserIdByBusinessConnectionId } from './users';
+import { handleMediaMessage } from './media-handler';
 
 // Глобальная переменная для отслеживания инициализации
 let botInitialized = false;
@@ -104,6 +105,18 @@ bot.command('demo', async (ctx) => {
 
 async function chat(ctx: Context, who: Who, message: Message) {
   console.log('chat.message:', JSON.stringify(message));
+  const userId = await getUserIdByBusinessConnectionId(ctx?.businessConnectionId||'')||0;
+  const mode = await getMode(message.chat.id);
+
+  if (message.photo || message.animation || message.document) {
+      try {
+          const id = mode === 'demo' ? message.chat.id : userId;
+          await handleMediaMessage(ctx, message, id);
+      } catch (e) {
+          console.error('Ошибка при отправке уведомления о медиа:', e);
+      }
+      return;
+  }
   
   if (message.voice) {
     const { file_id, duration, mime_type, file_size } = message.voice as TelegramVoice;
---
+++ src/media-handler.ts
@@ -0,0 +1,14 @@
+import {Context} from "grammy";
+import {Message} from "grammy/out/types";
+
+export async function handleMediaMessage (ctx: Context, message: Message, userId: number) {
+    const clientUsername = message.chat?.username;
+    const clientId = message.chat?.id;
+    let clientLink = clientUsername
+        ? `https://t.me/${clientUsername}`
+        : `tg://user?id=${clientId}`;
+
+    const notifyText = `Клиент прислал файл, который бот пока не умеет их обрабатывать, поэтому нужно ответить вручную. [Открыть чат с клиентом](${clientLink})`;
+
+    await ctx.api.sendMessage(userId, notifyText, { parse_mode: 'Markdown' });
+}
